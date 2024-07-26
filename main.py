7f2314127e18d3dd19bb0fd72a44af231bcc185c
mmal3k <mmalekrais@gmail.com>
2024-07-26 15:17:20 +0100

    web scrap time jobs websites

--- main.py
+++ main.py
@@ -1,12 +1,31 @@
 from bs4 import BeautifulSoup
+import requests
+import time
 
-with open("home.html","r") as html_file:
-    content = html_file.read()
-    # print(content)
-    
-    soup = BeautifulSoup(content , "lxml")
-    course_cards = soup.find_all("div" , class_ = "card")
-    for course in course_cards : 
-        course_name = course.h5.text
-        course_price = course.a.text.split()[-1]
-        print(f'{course_name} costs {course_price}')
+print("Put some skill that you are not familiar with")
+unfamiliar_skill = input(">")
+print(f"Filtering out {unfamiliar_skill}")
+def find_jobs(): 
+    url = "https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=python&txtLocation="
+    html_text = requests.get(url).text
+    soup  = BeautifulSoup(html_text , "lxml")
+    jobs  = soup.find_all("li",class_ = "clearfix job-bx wht-shd-bx")
+    for index , job in enumerate(jobs):
+        published_date = job.find("span" , class_ ="sim-posted").span.text
+        if "few" in published_date : 
+            company = job.find("h3" , class_="joblist-comp-name").text.strip()
+            skills =job.find("span", class_="srp-skills").text.replace(" ","").strip()
+            more_info = job.header.h2.a["href"]
+            if unfamiliar_skill not in skills : 
+                with open(f"posts/{index}.txt", "w") as f:
+                    f.write(f"company name : {company} \n")
+                    f.write(f"skills : {skills} \n")
+                    f.write(f"more_info : {more_info} \n")
+                print(f"File saved :{index}")
+
+if __name__ == "__main__":
+    while True : 
+        find_jobs()
+        time_wait = 10
+        print(f'waiting {time_wait} minutes...')
+        time.sleep(time_wait * 60)
